4b09c9e0217667008e16958aedc62eec4e7472e0
Jens W. Klein <jens@bluedynamics.com>
2012-07-02 22:06:11 +0200

    admin ui

--- src/souper/plone/browser/views.py
+++ src/souper/plone/browser/views.py
@@ -15,6 +15,7 @@ from souper.soup import (
 )
 from Products.Five import BrowserView
 
+
 class SoupAdmin(BrowserView):
 
     @property
@@ -28,8 +29,8 @@ class SoupAdmin(BrowserView):
             ret.append(name)
         return ret
 
-    def count(self, soup):
-        soup = getSoup(aq_inner(self.context), soup)
+    def count(self, sid):
+        soup = get_soup(sid, aq_inner(self.context))
         return len(soup.storage)
 
     def redirect_base(self, msg):
@@ -38,62 +39,60 @@ class SoupAdmin(BrowserView):
         self.request.RESPONSE.redirect(url)
 
     def storage_path(self, id):
-        try:
-            path = StorageLocator(self.context).path(id)
-        except KeyError, e:
-            path = '/ (not initialized yet)'
-        else:
-            if not path.startswith('/'):
-                path = '/' + path
+        locator = IStorageLocator(self.context)
+        path = locator.path(id)
+        if not path.startswith('/'):
+            path = '/' + path
         return path
 
     def reindex_soup(self):
-        id = self.request.form.get('id')
-        if not id:
-            return self.redirect_base('No id')
-        soup = getSoup(self.context, id)
+        sid = self.request.form.get('id')
+        if not sid:
+            return self.redirect_base('No soup id given!')
+        soup = get_soup(sid, self.context)
         soup.reindex()
-        msg = '%s reindexed.' % id
+        msg = '%s reindexed.' % sid
         return self.redirect_base(msg)
 
     def rebuild_soup(self):
-        id = self.request.form.get('id')
-        if not id:
-            return self.redirect_base(msg)
-        soup = getSoup(self.context, id)
+        sid = self.request.form.get('id')
+        if not sid:
+            return self.redirect_base('No soup id given')
+        soup = get_soup(sid, self.context)
         soup.rebuild()
         msg = '%s rebuilt.' % id
         return self.redirect_base(msg)
 
     def move_storage(self):
-        id = self.request.form.get('id')
-        if not id:
+        sid = self.request.form.get('id')
+        if not sid:
             return self.redirect_base(u'No id given')
         path = self.request.form.get('path')
         if not path:
             return self.redirect_base(u'No path given')
-        locator = StorageLocator(self.context)
+        locator = IStorageLocator(self.context)
         try:
-            target = locator.traverse(path)
+            locator.traverse(path)
         except ValueError:
-            return self.redirect_base(u'Desired path not exists')
+            return self.redirect_base(u'Desired path does not exist!')
         method = self.request.form.get('moveormount')
         if method == 'move':
-            locator.move(id, path)
+            locator.move(sid, path)
             transaction.commit()
-            return self.redirect_base(u'Moved storage of %s to %s' % (id, path))
+            return self.redirect_base(u'Moved storage of %s to %s' %
+                                      (id, path))
         elif method == "mount":
-            locator.relocate(id, path)
+            locator.set_path(sid, path)
             transaction.commit()
             return self.redirect_base(u'Mounted storage %s to %s' % (id, path))
         else:
             return self.redirect_base(u'Invalid action (move or mount only)')
 
     def rebuild_length(self):
-        id = self.request.form.get('id')
-        if not id:
-            return self.redirect_base(msg)
-        soup = getSoup(self.context, id)
+        sid = self.request.form.get('id')
+        if not sid:
+            return self.redirect_base('No soup id given')
+        soup = get_soup(sid, self.context)
         newlen = len(soup.storage.data)
         soup.storage.length.set(newlen)
         transaction.commit()
